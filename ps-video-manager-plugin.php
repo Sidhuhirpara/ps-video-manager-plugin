47ebc848c3fca807e3cd676a6d83e3703d186aae
Sidhuhirpara <siddharthhirpara108@gmail.com>
2026-02-27 16:03:43 +0530

    Connect custom post type class and initialize plugin

--- ps-video-manager-plugin.php
+++ ps-video-manager-plugin.php
@@ -18,3 +18,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 define( 'PSVM_VERSION', '1.0.0' );
 define( 'PSVM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'PSVM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+
+/**
+ * Include required files.
+ */
+require_once PSVM_PLUGIN_DIR . 'includes/class-psvm-post-type.php';
+
+/**
+ * Initialize plugin.
+ */
+function psvm_init() {
+	new PSVM_Post_Type();
+}
+add_action( 'plugins_loaded', 'psvm_init' );
